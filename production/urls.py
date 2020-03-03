e08c2e4d10d3457cd000052b34a88b38f482ea21
mrCrendel <tggrmi@gmail.com>
2020-03-03 22:09:07 +0600

    * fix product urls

--- production/urls.py
+++ production/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # path('<str:slug>/', production_views.one_c_single, name='one_c_single'),
 
     # path('online-cashbox/', production_views.online_cashbox_list, name='online_cashbox_list'),
-    path('online-cashbox/<int:id>', production_views.online_cashbox_single_old, name='online_cashbox_single_old'),
     path('online-cashbox/<str:slug>', production_views.online_cashbox_single, name='online_cashbox_single'),
+    path('online-cashbox/<int:id>', production_views.online_cashbox_single_old, name='online_cashbox_single_old'),
 
     path('electronic-signature/', production_views.electronic_signature_list, name='electronic_signature_list'),
     path('electronic-signature/<str:slug>/', production_views.electronic_signature_single, name='electronic_signature_single')
